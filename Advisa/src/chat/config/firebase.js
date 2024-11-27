bc99fb3dc70461d505ef1d303220dc676e899ca1
TharinduLala <tharindulalantha2001@gmail.com>
2024-11-27 19:25:06 +0530

    updated

--- Advisa/src/chat/config/firebase.js
+++ Advisa/src/chat/config/firebase.js
@@ -1,14 +1,31 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
 
 // Firebase config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBA4GMFRQQeTbQjE4t4XO1nGAvKS9qWkvk",
+//   authDomain: "advisa-1dd90.firebaseapp.com",
+//   projectId: "advisa-1dd90",
+//   storageBucket: "advisa-1dd90.firebasestorage.app",
+//   messagingSenderId: "644316079915",
+//   appId: "1:644316079915:web:679ffd97d7962985e1876e"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBA4GMFRQQeTbQjE4t4XO1nGAvKS9qWkvk",
-  authDomain: "advisa-1dd90.firebaseapp.com",
-  projectId: "advisa-1dd90",
-  storageBucket: "advisa-1dd90.firebasestorage.app",
-  messagingSenderId: "644316079915",
-  appId: "1:644316079915:web:679ffd97d7962985e1876e"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // initialize firebase
--- Advisa/src/config.js
+++ Advisa/src/config.js
@@ -1,7 +1,1 @@
-export const BASE_URL = 'http://192.168.43.30:3000/api';
-// export const API_KEY = 'AIzaSyCeQwWuhUaaNyKMWbrLitgqAV2KPsL1gfc';
-// export const AUTH_DOMAIN = 'expochatapp-1d698.firebaseapp.com';
-// export const PROJECT_ID = 'expochatapp-1d698';
-// export const STORAGE_BUCKET = 'expochatapp-1d698.firebasestorage.app';
-// export const MESSAGING_SENDER_ID = '526079954361';
-// export const APP_ID = '1:526079954361:web:fc03c37ebd15c86e697a68';
+export const BASE_URL = 'http://192.168.1.13:3000/api';
